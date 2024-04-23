5298d015755e7918968843bfab538446e095088a
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-22 17:03:51 -0700

    07 fn

--- 07_multiple_letter_count.py
+++ 07_multiple_letter_count.py
@@ -6,4 +6,20 @@ def multiple_letter_count(phrase):
 
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
+
+        loop through every letter in phrase
+        if it exists, add 1
+        else, set it equal to 1
+
+        value in dictionary || 0
+        +1
+
+
     """
+
+    dict = {}
+
+    for ltr in phrase:
+        dict[ltr] = dict.get(ltr, 0)+1
+
+    return dict
