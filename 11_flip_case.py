033c29d67cd39d222d2dabcd24fb0087e0b2c1ec
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-22 18:21:39 -0700

    011 fn

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -10,4 +10,30 @@ def flip_case(phrase, to_swap):
         >>> flip_case('Aaaahhh', 'h')
         'AaaaHHH'
 
+    check if the letter.lower = to_swap lower
+    check if letter is upper, switch to lower
+    if letter is lower, switch to upper
+
+    find the char and replace with lower
+    .replace
+
+    helloH
+    aelloH
+    aelloh
+    Helloh
+
+    return phrase.replace(to_swap.lower, to_swap.upper)
     """
+
+    phrase_list = list(phrase)
+
+    changed_list = []
+
+    for char in phrase_list:
+        if char.lower() == to_swap.lower():
+
+            char = char.lower() if char == char.upper() else char.upper()
+
+            changed_list.append(char)
+
+    return "".join(changed_list)
