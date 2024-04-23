efd211b334f3f0cffe6891af872960fafb91e644
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-23 12:07:03 -0700

    15

--- 15_intersection.py
+++ 15_intersection.py
@@ -1,15 +1,19 @@
 def intersection(l1, l2):
     """Return intersection of two lists as a new list::
-    
+
         >>> intersection([1, 2, 3], [2, 3, 4])
         [2, 3]
-        
+
         >>> intersection([1, 2, 3], [1, 2, 3, 4])
         [1, 2, 3]
-        
+
         >>> intersection([1, 2, 3], [3, 4])
         [3]
-        
+
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+
+    # if the value in l1 exists in l2, put it in a list
+
+    return list(set(l1) & set(l2))
